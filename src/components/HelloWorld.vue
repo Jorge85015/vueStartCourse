97b4b9e4e2c6f07fd3f22b7aa026429195c5aeaf
unknown <jorge.quinonez@alumnos.uneatlantico.es>
2023-04-03 15:03:06 +0200

    feature: list and conditionals code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,121 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="mainPage">
+      <h1>Hello {{ students.name }}</h1> <!--Interpolation-->
+      <a :href="github" target="blank" rel="nooperner noreferrer"><h2>Github</h2></a>
+      <a v-bind:href="twitter" target="blank"><h2>Twitter</h2></a>
+      <br>
+      <h4>Student List</h4>
+      <ul class="list-of-students">
+        <li v-for="student in students" :key="student.id">
+          <a @click="selectedStudent = student" :class="{ 'is-active': selectedStudent === student }"> <!--Event binding-->
+            <span>{{ student.name }} {{ student.surname }}</span>
+          </a>
+        </li>  <!-- Property binding shortcut : -->
+      </ul>
+      <p>student information:</p>
+      <div v-if="selectedStudent">  <!-- Conditional rendering -->
+        <div>
+          <label class="label" for="id">id: </label>
+          <label class="input" id="id">{{ selectedStudent.id }}</label><br>
+        </div>
+        <div>
+          <label class="label" for="name">name: </label>
+          <input type="input" v-model="selectedStudent.name"><br/>
+        </div>
+        <div>
+          <label for="show" class="checkbox">
+            show more
+            <input type="checkbox" id="show" v-model="showMore"/>
+          </label>
+        </div>
+        <div  v-show="showMore">
+          <label class="label" for="id">surname: </label>
+          <input type="input" v-model="selectedStudent.surname"/><br>
+        </div>
+        <div v-show="showMore">
+          <label class="label" for="age" >age: </label>
+          <input type="input" v-model="selectedStudent.age"/><br>
+        </div>
+
+      </div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: "HelloWorld",
+  data() {
+    return {
+      selectedStudent: undefined,
+      showMore: false,
+      students: [
+        {
+          id: 1,
+          name: "Jorge",
+          surname: "Quinonez",
+          age: 19,
+          carreer: "Software Engineer",
+          nationality: "Guatemala",
+          eyesColor: "",
+          grade: "",
+          active: true,
+          description: "",
+        },
+        {
+          id: 2,
+          name: "Qory",
+          surname: "Centeno",
+          age: 21,
+          carreer: "Psicology",
+          nationality: "Peru",
+          eyesColor: "",
+          grade: "",
+          active: true,
+          description: "",
+        },
+        {
+          id: 3,
+          name: "Tomi",
+          surname: "Marcelo",
+          age: 19,
+          carreer: "Arquitecture",
+          nationality: "Spain",
+          eyesColor: "",
+          grade: "",
+          active: false,
+          description: "",
+        }
+      ],
+      message: 'Student information:',
+    }
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+p {
+  font-weight: bold;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.colored-line {
+  width: 160px;
+  height: 8px;
+  border: 1px #dbdbdb solid;
+  margin: 8px 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+span:hover {
+  cursor: pointer;
+  color: rgb(15, 21, 182);
 }
-a {
-  color: #42b983;
+
+.is-active {
+  background-color: blue;
+  color: white;
 }
 </style>
+
+
+
+
